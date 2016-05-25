b0a553b6f0a13c06c5306b44f2aed826d8a19490
Dinh Tran <trandtb@greenglobal.vn>
2016-05-25 17:17:37 +0700

    add VaiTro, Quyen

--- src/main/java/vn/cal/model/Quyen.java
+++ src/main/java/vn/cal/model/Quyen.java
@@ -3,12 +3,110 @@ package vn.cal.model;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.springframework.beans.factory.annotation.Value;
+
+import vn.cal.core.Service;
+
+
 public class Quyen extends HashMap<String, Boolean>{
 
     public static transient final Logger log = Logger.getLogger(Quyen.class.getName());
     
     private static final long serialVersionUID = 8664211408151370739L;
     
-    
-    
+    @Value("${action.xem}")
+    public String XEM = "";
+    @Value("${action.lietke}")
+	public String LIST = ""; // duoc vao trang list search url
+	@Value("${action.sua}")
+	public String SUA = ""; // duoc sua bat ky
+	@Value("${action.xoa}")
+	public String XOA = ""; // duoc xoa bat ky
+	@Value("${action.them}")
+	public String THEM = ""; // duoc them
+	
+	public char CHAR_CACH = ':';
+	public String CACH = CHAR_CACH + "";
+	
+	@Value("${url.vaitro}")
+	public String VAITRO = "";
+	@Value("${url.vaitro}" + ":" + "${action.xem}")	
+	public String VAITROXEM;
+	@Value("${url.vaitro}" + ":" + "${action.them}")	
+	public String VAITROTHEM = "";
+	@Value("${url.vaitro}" + ":" + "${action.list}")	
+	public String VAITROLIST = "";
+	@Value("${url.vaitro}" + ":" + "${action.xoa}")	
+	public String VAITROXOA = "";
+	@Value("${url.vaitro}" + ":" + "${action.sua}")	
+	public String VAITROSUA = "";	
+	
+	@Value("${url.nguoidung}")
+	public String NGUOIDUNG = "";
+	@Value("${url.nguoidung}" + ":" + "${action.xem}")	
+	public String NGUOIDUNGXEM = "";
+	@Value("${url.nguoidung}" + ":" + "${action.them}")
+	public String NGUOIDUNGTHEM = "";
+	@Value("${url.nguoidung}" + ":" + "${action.list}")
+	public String NGUOIDUNGLIST = "";
+	@Value("${url.nguoidung}" + ":" + "${action.xoa}")
+	public String NGUOIDUNGXOA = "";
+	@Value("${url.nguoidung}" + ":" + "${action.sua}")
+	public String NGUOIDUNGSUA = "";
+	
+	public String[] getResources(){
+		return new String[] {VAITRO, NGUOIDUNG};
+	}
+	
+	public String[] getActions(){
+		return new String[] {LIST, XEM, THEM, SUA, XOA};
+	}
+	
+	private String resource = "";
+	private long id;
+	private User nguoiTao;
+	private transient AuthorizingRealm realm;
+	
+	public Quyen(AuthorizingRealm realm) {
+		this.realm = realm;
+	}
+	
+	public Quyen(AuthorizingRealm realm, String resource){
+		this(realm);
+		this.resource = resource;
+	}
+
+	public Quyen(AuthorizingRealm realm, String resource, long id, User nguoiTao) {
+		this(realm, resource);
+		this.id = id;
+		this.nguoiTao = nguoiTao;
+	}
+	
+	public Quyen() {
+		super();
+	}
+
+	public Boolean get(Object key_){
+		if(key_ == null){
+			return false;
+		}
+		
+		if (id != 0 && nguoiTao != null && nguoiTao.equals(new Service<>().getUserService().getUser(false))) {
+			return true;
+		}
+		
+		String key = key_.toString();
+		if(!key.isEmpty() && key.charAt(0) == '_'){
+			key = resource + key;
+		}
+		if(id!=0){
+			key += CACH + id;
+		}
+		log.info(key);
+		boolean result = realm.isPermitted(null, key.replace('_', CHAR_CACH));
+		return result;
+	}
+	
+	
 }
--- src/main/java/vn/cal/model/VaiTro.java
+++ src/main/java/vn/cal/model/VaiTro.java
@@ -1,14 +1,24 @@
 
 package vn.cal.model;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Logger;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Index;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
+
+import org.zkoss.bind.BindUtils;
+import org.zkoss.zul.TreeNode;
+
+import com.google.common.base.Strings;
 
 import vn.cal.core.Model;
 
@@ -20,13 +30,14 @@ public class VaiTro extends Model<VaiTro> {
     public static transient final Logger log = Logger.getLogger(VaiTro.class.getName());
     
     public static final String QUANTRIVIEN = "quantrivien";
+    public static final String NGUOIDUNG = "nguoidung";
     
-    public static final String[] VAITRO_DEFAULTS = { QUANTRIVIEN };
+    public static final String[] VAITRO_DEFAULTS = { QUANTRIVIEN, NGUOIDUNG };
     
     private String tenVaiTro = "";
     private String alias = "";
     private Set<String> quyens = new HashSet<>();
-    private Set<String> quyenEdits = quyens;
+	private Set<String> quyenEdits = quyens;
     
     public VaiTro(){
         super();
@@ -43,7 +54,7 @@ public class VaiTro extends Model<VaiTro> {
     }
     
     public void setTenVaiTro(String tenVaiTro) {
-        this.tenVaiTro = tenVaiTro;
+        this.tenVaiTro = Strings.nullToEmpty(tenVaiTro);
     }
     
     public String getAlias() {
@@ -51,9 +62,109 @@ public class VaiTro extends Model<VaiTro> {
     }
     
     public void setAlias(String alias) {
-        this.alias = alias;
+        this.alias = Strings.nullToEmpty(alias);
     }
     
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name="vaitro_quyens")
+    public Set<String> getQuyens() {
+    	if(quyens.isEmpty()){
+    		quyens.addAll(getQuyenMacDinhs());
+    	}
+		return quyens;
+	}
+	
+	public void setQuyens(Set<String> quyens) {
+		this.quyens = quyens;
+	}
+
+    @Transient
+	private Set<String> getQuyenMacDinhs() {
+		return getQuyenMacDinhs(getAlias());
+	}
+
+    @Transient
+	private Set<String> getQuyenMacDinhs(String alias) {
+		Set<String> quyens = new HashSet<>();
+		if(!alias.isEmpty()){
+			Quyen q = new Quyen();
+			
+			if(NGUOIDUNG.equals(alias)){
+				
+				quyens.add(q.NGUOIDUNGLIST);
+				quyens.add(q.NGUOIDUNGXEM);
+				quyens.add(q.NGUOIDUNGSUA);
+				quyens.add(q.NGUOIDUNGXOA);
+				quyens.add(q.NGUOIDUNGTHEM);
+				
+			} else if(QUANTRIVIEN.equals(alias)){
+				
+				quyens.add(q.VAITROLIST);
+				quyens.add(q.VAITROXEM);
+				quyens.add(q.VAITROSUA);
+				quyens.add(q.VAITROXOA);
+				quyens.add(q.VAITROTHEM);
+				
+				quyens.add(q.NGUOIDUNGLIST);
+				quyens.add(q.NGUOIDUNGXEM);
+				quyens.add(q.NGUOIDUNGSUA);
+				quyens.add(q.NGUOIDUNGXOA);
+				quyens.add(q.NGUOIDUNGTHEM);
+			}
+		}
+    	return quyens;
+	}
+
+    private Set<TreeNode<String[]>> selectedItems = new HashSet<>();
+    
+    @Transient
+    public Set<TreeNode<String[]>> getSelectedItems(){
+    	return selectedItems;
+    }
+    
+    public void setSelectedItems(Set<TreeNode<String[]>> selectedItems_){
+    	Set<TreeNode<String[]>> selectedItemsTmp = new HashSet<>();
+    	selectedItemsTmp.addAll(selectedItems);
+    	
+    	for (final TreeNode<String[]> node : selectedItems) {
+			if(!selectedItems_.contains(node)){
+				quyenEdits.remove(node.getData()[1]);
+				selectedItemsTmp.remove(node);
+				
+				//Remove parent
+				TreeNode<String[]> pNode = node.getParent();
+				if(pNode!=null && selectedItems_.contains(pNode)){
+					quyenEdits.remove(pNode.getData()[1]);
+					selectedItemsTmp.remove(pNode);
+				}
+				
+				//Remove all children
+				if(node.getChildCount() > 0){
+					for (TreeNode<String[]> n : node.getChildren()) {
+						quyenEdits.remove(n.getData()[1]);
+						selectedItemsTmp.remove(n);
+					}
+				}
+			}
+		}
+    	
+    	for (TreeNode<String[]> node : selectedItems_) {
+    		if (!selectedItems.contains(node)) {
+				quyenEdits.add(node.getData()[1]);
+				selectedItemsTmp.add(node);
+				if (node.getChildCount() > 0) {
+					for (TreeNode<String[]> n : node.getChildren()) {
+						quyenEdits.add(n.getData()[1]);
+						selectedItemsTmp.add(n);
+					}
+				}
+			}
+		}
+    	
+    	selectedItems = selectedItemsTmp;
+		BindUtils.postNotifyChange(null, null, this, "quyenEdits");
+		BindUtils.postNotifyChange(null, null, this, "selectedItems");
+    }
     
     
 }
